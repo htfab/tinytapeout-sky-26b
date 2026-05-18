90e4495d49a66fa6018e1a2788aea6844395650d
TinyTapeoutBot <139130078+TinyTapeoutBot@users.noreply.github.com>
2026-05-18 08:06:15 +0300

    feat: update project tt_um_RO_aging from RamonSsc/tt_um_Onchip_Ring_Oscillators_Aging
    Commit: e2b28d81c0efe9ee007cdd5b45976d0eaa6281eb
    Workflow: https://github.com/RamonSsc/tt_um_Onchip_Ring_Oscillators_Aging/actions/runs/26014511497

---
+++ projects/tt_um_RO_aging/docs/info.md
@@ -0,0 +1,20 @@
+<!---
+
+This file is used to generate your project datasheet. Please fill in the information below and delete any unused
+sections.
+
+You can also include images in this folder and reference them in the markdown. Each image must be less than
+512 kb in size, and the combined size of all images must be less than 1 MB.
+-->
+
+## How it works
+
+the design and implementation of Ring Oscillators (ROs) to evaluate aging effects in the Skywater130 process node. The project is part of UNICASS-2024 and focuses on measuring performance degradation caused by aging mechanisms such as Bias Temperature Instability (BTI) and Hot Carrier Injection (HCI).The ring oscillator circuits were implemented in two configurations, 13 and 101 stages, both with two load conditions tested using transmission gates for the device under test (DUT) in rovcell 1. It is important to note that the inverters were designed to maintain equal rise and fall times.
+
+## How to test
+
+Power it up
+
+## External hardware
+
+Various inputs
---
+++ projects/tt_um_RO_aging/tt_um_RO_aging.v
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2024 Your Name
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_RO_aging(
+    input  wire       VGND,
+    input  wire       VDPWR,    // 1.8v power supply
+//    input  wire       VAPWR,    // 3.3v power supply
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    inout  wire [7:0] ua,       // Analog pins, only ua[5:0] can be used
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
+);
+
+    Ro_Aging Ro_Aging(
+        .VDPWR(VDPWR),
+        .VGND(VGND),
+        .Output(ua[0]),
+        .DUT_Gate(ua[1]),
+        .DUT_Footer(ua[2]),
+        .DUT_Header(ua[3]),
+        .Drain_Sense(ua[4]),
+        .Drain_Force(ua[5]),
+        .Ro_control(ui_in[0]),
+        .Not_Ro_control(ui_in[1])
+    );
+
+
+    // outputs 
+    assign uo_out[0] = VGND;
+    assign uo_out[1] = VGND;
+    assign uo_out[2] = VGND;
+    assign uo_out[3] = VGND;
+    assign uo_out[4] = VGND;
+    assign uo_out[5] = VGND;
+    assign uo_out[6] = VGND;
+    assign uo_out[7] = VGND;
+
+    // ties for the output enables
+    assign uio_out[0] = VGND;
+    assign uio_out[1] = VGND;
+    assign uio_out[2] = VGND;
+    assign uio_out[3] = VGND;
+    assign uio_out[4] = VGND;
+    assign uio_out[5] = VGND;
+    assign uio_out[6] = VGND;
+    assign uio_out[7] = VGND;
+
+    assign uio_oe[0] = VGND;
+    assign uio_oe[1] = VGND;
+    assign uio_oe[2] = VGND;
+    assign uio_oe[3] = VGND;
+    assign uio_oe[4] = VGND;
+    assign uio_oe[5] = VGND;
+    assign uio_oe[6] = VGND;
+    assign uio_oe[7] = VGND;
+
+    // List all unused inputs to prevent warnings
+    wire _unused = &{ena, clk, rst_n, uio_in[7:0], ui_in[7:2], 1'b0};
+
+
+endmodule
